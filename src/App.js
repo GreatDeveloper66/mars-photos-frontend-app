64aa97ba83133a9693b2715ef1b50a87c7dde1dd
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-05 19:41:07 -0500

    Install isomorphic fetch and react router dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Mars from './Components/Mars'
+//import Register from './Components/Register'
+//import SignIn from './Components/SignIn'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path ='/' component={ Mars } />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default App

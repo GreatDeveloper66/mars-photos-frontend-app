23787b8f73e56573f3d3674b6079837424982589
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-05 19:53:21 -0500

    Remove unused component import

--- src/Components/Mars.js
+++ src/Components/Mars.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import { Row, Container, Col, Form, Button, Image } from 'react-bootstrap'
 import { URL } from './EnvVars'
 import fetch from 'isomorphic-fetch'
